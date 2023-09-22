c8a636294e9329765c6bffe2ee0a334b57af51c5
muhammadazzam321 <azzamizzuddin123@gmail.com>
2023-09-22 21:20:28 +0700

    update home

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,26 +5,130 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Future<void> _showMyDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Logout App Surat',
+            style: poppinsTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+              color: blackColor,
+            ),
+          ),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  'Apakah anda yakin ingin keluar',
+                  style: poppinsTextStyle.copyWith(
+                    fontSize: 14,
+                    fontWeight: medium,
+                    color: thirdColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                'Kembali',
+                style: poppinsTextStyle.copyWith(
+                  fontSize: 12,
+                  fontWeight: medium,
+                  color: blackColor,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Color(0xffc51f1a))),
+              onPressed: () {},
+              child: Text('Keluar',
+                  style: poppinsTextStyle.copyWith(
+                      fontSize: 12, fontWeight: medium, color: whiteColor)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _title() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Selamat Datang',
+          style: poppinsTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+            color: blackColor,
+          ),
+        ),
+        Text(
+          'Sistem Informasi Pengelolaan Surat \n Bagian Protokol Kantor Walikota Palembang',
+          style: poppinsTextStyle.copyWith(
+            fontSize: 13,
+            fontWeight: medium,
+            color: grayColor,
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        leading: Builder(
-            builder: (context) => IconButton(
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                icon: Icon(
-                  Icons.menu,
-                  color: primaryColor,
-                ))),
-      ),
-      drawer: NavBar(),
-      body: Center(
-        child: Text('Main Page'),
-      ),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: IconButton(
+                  onPressed: () {
+                    _showMyDialog(context);
+                  },
+                  icon: Icon(
+                    Icons.logout_outlined,
+                    color: primaryColor,
+                    size: 30,
+                  )),
+            )
+          ],
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Builder(
+                builder: (context) => IconButton(
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                    icon: Icon(
+                      Icons.menu,
+                      size: 30,
+                      color: primaryColor,
+                    ))),
+          ),
+        ),
+        drawer: const NavBar(),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin1),
+          child: Column(
+            children: [
+              _title(),
+            ],
+          ),
+        ));
   }
 }
